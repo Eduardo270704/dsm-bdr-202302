33880cff74d81167967b4b5bdb66469b8632e638
Eduardo Moraes <30940593+Eduardo270704@users.noreply.github.com>
2023-08-15 23:03:22 -0300

    aula03.sql

--- aula03.sql
+++ aula03.sql
@@ -1,1 +1,29 @@
 create database bd_aula03;
+
+\c bd_aula03
+
+create table tbl_cliente (
+	codigo_cliente integer primary key,
+	Nome varchar not null,
+	Cidade varchar,
+	Endereco varchar
+);
+
+create table tbl_titulo (
+	codigo_titulo integer primary key,
+	titulo varchar Not null,
+	descricao varchar,
+	categoria text
+);
+
+create table tbl_emprestimo (
+	numero_cliente integer primary key,
+	codigo_cliente integer,
+	codigo_livro integer
+);
+
+create table tbl_livros (
+	cod_livro integer primary key,
+	codigo_titulo integer,
+	status text
+);
